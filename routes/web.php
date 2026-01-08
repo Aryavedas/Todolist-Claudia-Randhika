17082e56d07e18852bb72c673f00271d27a16898
Arya Veda <vedakhansa@gmail.com>
2026-01-08 23:38:04 +0700

    add route setup

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TodoController;
+use Illuminate\Support\Facades\Artisan;
 
 // Home setelah login
 Route::get('/', [TodoController::class, 'index'])
@@ -9,9 +10,20 @@ Route::get('/', [TodoController::class, 'index'])
     ->name('todos.index');
 
 // Update toggle checkbox
-Route::put('/update-toggle/{todo}', 
+Route::put(
+    '/update-toggle/{todo}',
     [TodoController::class, 'updateByClickToggle']
 )->middleware(['auth'])->name('home.todos.update');
+
+Route::get('/setup-database', function () {
+    try {
+        // Menjalankan migrasi database
+        Artisan::call('migrate --force');
+        return 'Sukses! Database berhasil dibuat. Silakan hapus route ini dan coba Register lagi.';
+    } catch (\Exception $e) {
+        return 'Gagal: ' . $e->getMessage();
+    }
+});
 
 // CRUD TODOLIST
 require __DIR__ . '/web/todos.php';

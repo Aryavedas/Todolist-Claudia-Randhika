1fc33b2d5267dfa9757dd5c76ef5ddee13f68288
Arya Veda <vedakhansa@gmail.com>
2026-01-08 23:24:35 +0700

    config app service provider to https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Paksa HTTPS jika di environment production (atau di Vercel)
+        if ($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
